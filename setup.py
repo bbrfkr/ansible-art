b495252812308191ac3dde5d5b62b77dd6bda555
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-06-25 22:38:04 +0900

    ansible-art ver.0.1

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def main():
+    description = 'A simple tool to apply role of ansible'
+
+    setup(
+        name='ansible-art',
+        version='0.1',
+        author='Tatsunori Saito',
+        author_email='bbrfkr@gmail.com',
+        url='http://tracks.bbrfkr.mydns.jp',
+        description=description,
+        long_description=description,
+        zip_safe=False,
+        include_package_data=True,
+        packages=['ansibleart'],
+        package_dir={'ansibleart': 'ansibleart'},
+        package_data={'ansibleart': ['data/*']},
+        install_requires=[],
+        scripts=['ansibleart/bin/ansible-art'],
+        tests_require=[],
+        setup_requires=[],
+    )
+
+
+if __name__ == '__main__':
+    main()
