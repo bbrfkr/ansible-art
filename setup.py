e4ef6edd8ada5b10529a7e6106e0198949857c2d
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-07-03 13:11:30 +0900

    change version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def main():
 
     setup(
         name='ansible-art',
-        version='0.3',
+        version='0.3.1',
         author='Tatsunori Saito',
         author_email='bbrfkr@gmail.com',
         url='https://github.com/bbrfkr/ansible-art',
