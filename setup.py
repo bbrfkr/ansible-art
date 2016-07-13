86751172a69b603aaf43de9f15b03f057927622a
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-07-13 20:03:36 +0900

    change inventory file name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def main():
 
     setup(
         name='ansible-art',
-        version='0.3.1',
+        version='0.3.2',
         author='Tatsunori Saito',
         author_email='bbrfkr@gmail.com',
         url='https://github.com/bbrfkr/ansible-art',
